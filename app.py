c1ed0c19a176fa1a80e5d9a024e8b9833ad19ca8
ANJANA DEVI S <23d104@psgitech.ac.in>
2026-02-18 00:09:55 +0530

    Change title from 'Simple Calculator' to 'Advanced Calculator'

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Simple Calculator")
+st.title("Adavanced Calculator")
 
 num1 = st.number_input("Enter first number")
 num2 = st.number_input("Enter second number")
