09c2bbb58e3c813681a3a9d9f01012c916b1fdbc
ANJANA DEVI S <23d104@psgitech.ac.in>
2026-04-15 01:15:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Adavanced Calculator")
+st.title("Simple Calculator")
 
 num1 = st.number_input("Enter first number")
 num2 = st.number_input("Enter second number")
